40eae028415f128a77f56eed4b47d96d5eee59ea
Clayton Knittel <cknit1999@gmail.com>
2024-04-22 23:34:53 -0700

    add client subdirectory

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import { AsyncSocketContext } from "client/util/async_sockets";
+// import { isOk } from "client/util/status";
+// import { Test } from "proto/game_state";
+
+// const socket: OnoroSocket = new AsyncSocketContext(
+//   "ws://[::]:2345/onoro",
+//   true,
+// );
+
+export function App() {
+  return <div>Hello there.</div>;
+}
