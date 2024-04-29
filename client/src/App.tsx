e9a1c372cd1624376e2575697981c5701af4fafd
Clayton Knittel <cknit1999@gmail.com>
2024-04-28 22:23:00 -0700

    rm refs to wrong projects

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 
-import { ServerButton } from './ServerButton';
-
-// import { AsyncSocketContext } from "client/util/async_sockets";
-// import { isOk } from "client/util/status";
-// import { Test } from "proto/test";
-
-// const socket: OnoroSocket = new AsyncSocketContext(
-//   "ws://[::]:2345/onoro",
-//   true,
-// );
+import { ServerButton } from 'client/ServerButton';
 
 export function App() {
   return <ServerButton />;
