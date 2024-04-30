cb597119da4587b699c9c00e8c3201e9822e5232
Clayton Knittel <cknit1999@gmail.com>
2024-04-30 16:25:38 -0700

    disable cache for dev builds

--- server/src/static_file_server.rs
+++ server/src/static_file_server.rs
@@ -1,11 +1,19 @@
 use std::{fs, net::SocketAddr};
 
 use tokio::task::JoinHandle;
-use warp::Filter;
+use warp::{
+  http::HeaderValue,
+  reply::{self, Reply},
+  Filter,
+};
 
 use crate::security::{CERTFILE, KEYFILE};
 
 const DIR: &str = "../client/dist/dev/static";
+
+fn disable_cache(reply: impl Reply) -> impl Reply {
+  reply::with_header(reply, "cache_control", HeaderValue::from_static("no-cache"))
+}
 
 pub fn run_file_server(prod: bool, addr: SocketAddr) -> JoinHandle<()> {
   tokio::spawn(async move {
@@ -16,8 +24,8 @@ pub fn run_file_server(prod: bool, addr: SocketAddr) -> JoinHandle<()> {
 
     let directory = fs::canonicalize(DIR).unwrap();
     let route = warp::get().and(warp::fs::dir(directory));
-    let server = warp::serve(route);
     if prod {
+      let server = warp::serve(route);
       server
         .tls()
         .cert_path(CERTFILE)
@@ -25,6 +33,8 @@ pub fn run_file_server(prod: bool, addr: SocketAddr) -> JoinHandle<()> {
         .run(addr)
         .await
     } else {
+      let route = route.map(disable_cache);
+      let server = warp::serve(route);
       server.run(addr).await
     }
   })
