63c93db621583cc46931852a3715c9fbb4616b3f
Clayton Knittel <cknit1999@gmail.com>
2024-05-02 22:48:51 -0700

    rm unused code in proto

--- server/src/proto.rs
+++ server/src/proto.rs
@@ -1,28 +1,6 @@
-use serde::{
-  de::{self, Visitor},
-  Deserialize, Serialize,
-};
-use tokio_util::bytes::BytesMut;
+use serde::{de, Deserialize, Serialize};
 
 include!(concat!(env!("OUT_DIR"), "/mc_server.proto.rs"));
-
-#[derive(Debug)]
-struct BytesMutVisitor;
-
-impl<'de> Visitor<'de> for BytesMutVisitor {
-  type Value = BytesMut;
-
-  fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-    formatter.write_str("Expecting bytes")
-  }
-
-  fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
-  where
-    E: serde::de::Error,
-  {
-    Ok(BytesMut::from(v))
-  }
-}
 
 impl Serialize for ServerState {
   fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
