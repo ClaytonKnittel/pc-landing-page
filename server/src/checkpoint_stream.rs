30d2215993437484157eb067e8d047d64408fbd9
Clayton Knittel <cknit1999@gmail.com>
2024-05-19 07:15:06 +0000

    ckpt stream update

--- server/src/checkpoint_stream.rs
+++ server/src/checkpoint_stream.rs
@@ -1,15 +1,14 @@
-use std::{error, ops::DerefMut, time::Duration};
+use std::{ops::DerefMut, time::Duration};
 
-use async_bincode::tokio::AsyncBincodeStream;
-use bincode::{DefaultOptions, Serializer};
-use futures_util::{SinkExt, Stream, StreamExt};
 use serde::Serialize;
 use tokio::{
-  io::AsyncWrite,
+  io::{AsyncWrite, AsyncWriteExt},
   sync::Mutex,
   task::JoinHandle,
   time::{interval, MissedTickBehavior},
 };
+
+use crate::error::ThreadSafeError;
 
 pub trait IncrementalUpdate: Serialize {
   /// Should return true when there is new uncommitted incremental state to be
@@ -18,7 +17,11 @@ pub trait IncrementalUpdate: Serialize {
 
   /// Called after the incremental state has been checkpointed. This should
   /// commit all incremental state to the full state.
-  fn committed(&mut self);
+  fn commit(&mut self) -> Result<Vec<u8>, Box<dyn ThreadSafeError>>;
+
+  /// Don't love this API, think of better way (want to ser/de here, not in
+  /// impls).
+  fn recover(&mut self, increment_encoding: Vec<u8>) -> Result<(), Box<dyn ThreadSafeError>>;
 }
 
 pub struct CheckpointStreamOptions {
@@ -29,7 +32,7 @@ pub struct CheckpointStreamOptions {
 impl CheckpointStreamOptions {
   pub fn build<W, S>(self, checkpoint_writer: W, state: Mutex<S>) -> CheckpointStream<W, S>
   where
-    W: AsyncWrite + Send + 'static,
+    W: AsyncWrite + Unpin + Send + 'static,
     S: IncrementalUpdate + Send + Sync + 'static,
   {
     CheckpointStream::from_options(checkpoint_writer, state, self)
@@ -52,7 +55,7 @@ pub struct CheckpointStream<W, S> {
 
 impl<W, S> CheckpointStream<W, S>
 where
-  W: AsyncWrite + Send + 'static,
+  W: AsyncWriteExt + Unpin + Send + 'static,
   S: IncrementalUpdate + Send + Sync + 'static,
 {
   pub fn new(checkpoint_writer: W, state: Mutex<S>) -> Self {
@@ -70,26 +73,27 @@ where
   /// Starts a separate thread to run the checkpoint stream in. This will
   /// periodically poll `state`, and update the checkpointed state whenever
   /// there is a change.
-  pub fn start(mut self) -> JoinHandle<()> {
+  pub fn start(mut self) -> JoinHandle<Result<(), Box<dyn ThreadSafeError>>> {
     tokio::spawn(async move {
       let mut timer = interval(self.options.poll_period);
       timer.set_missed_tick_behavior(MissedTickBehavior::Skip);
 
       loop {
         timer.tick().await;
-        self.tick().await;
+        self.tick().await?;
       }
     })
   }
 
-  async fn tick(&mut self) -> Result<(), Box<dyn error::Error>> {
+  async fn tick(&mut self) -> Result<(), Box<dyn ThreadSafeError>> {
     let mut guard = self.state.lock().await;
     let state = guard.deref_mut();
 
     if state.has_update() {
-      // bincode::serialize_into(&mut self.checkpoint_writer, todo!())?;
-      let mut serializer = Serializer::new(&mut self.checkpoint_writer, DefaultOptions::default());
-      serde::Serialize::serialize(value, &mut serializer);
+      // let mut encoding = bincode::serialize(&state)?;
+      let mut encoding = state.commit()?;
+      self.checkpoint_writer.write_all(&mut encoding).await?;
+      self.checkpoint_writer.flush().await?;
     }
 
     Ok(())
